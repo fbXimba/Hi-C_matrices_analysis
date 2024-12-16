c5b6b52c74cd48fd2cc963c2178f2e694507edb5
fbXimba <francescabechere@gmail.com>
2024-12-16 10:43:42 +0100

    new structure: utils

--- complex.py
+++ complex.py
@@ -6,13 +6,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 #from scipy.stats import pearsonr
-import math
+#import math
 #import random as rnd
 import networkx as nx
 import time as tm
-from multiprocessing import Pool
-from tqdm import tqdm 
+#from multiprocessing import Pool
+#from tqdm import tqdm 
 from numpy import linalg as LA
+import utils as utils
 
 #NOTE: This code is currently a work in progress in its initial stages. NO BITCHING.
 
@@ -36,30 +37,21 @@ dfKBM=pd.read_csv(dir_data+"raw_KBM7_1Mb.csv",header=None)
 dataKBM0=dfKBM.to_numpy()
 
 #%%
-#Data cleaning
+#Data cleaning:
+t1=tm.time()
 
-#removing Y crhomosome from data
-Y = list(range(2892,2951))
+#removing Y crhomosome + isolated nodes from data
+#Y = list(range(2892,2951))
 
 #GM12878
-dataGM = np.delete(dataGM0,Y, axis=0)
-dataGM = np.delete(dataGM,Y, axis=1)
+dataGM=utils.data_cleaning(dataGM0)
 
 #KBM7
-dataKBM = np.delete(dataKBM0,Y, axis=0)
-dataKBM = np.delete(dataKBM,Y, axis=1)
+dataKBM=utils.data_cleaning(dataKBM0)
 
-#removing isolated nodes
-#GM12878
-dataGM = dataGM[~np.all(dataGM == 0, axis=1)]
-dataGM = dataGM[:, ~np.all(dataGM == 0, axis=0)]
-
-#KBM7
-dataKBM = dataKBM[~np.all(dataKBM == 0, axis=1)]
-dataKBM = dataKBM[:, ~np.all(dataKBM == 0, axis=0)]
-
-#log_scale for better visualization
+#normalization = log_scale for better visualization
 #log1p does log10(1+x) in order to avoid problems where there's a 0
+
 #GM12878
 dataGM_normalized = np.log1p(dataGM)
 
@@ -82,13 +74,11 @@ G_KBM= nx.from_numpy_array(dataKBM_normalized)
 t3=tm.time()
 
 #GM12878
-weights_GM={node: sum(weight for _, _, weight in G_GM.edges(node, data='weight')) for node in G_GM.nodes} 
-str_GM=np.fromiter(weights_GM.values(), dtype=float)
+str_GM=utils.stregth(G_GM)
 print(np.min(str_GM), np.max(str_GM), np.mean(str_GM)) #check min and max values
 
 #KBM7
-weights_KBM={node: sum(weight for _, _, weight in G_KBM.edges(node, data='weight')) for node in G_KBM.nodes}
-str_KBM=np.fromiter(weights_KBM.values(), dtype=float)
+str_KBM=utils.stregth(G_KBM)
 print(np.min(str_KBM), np.max(str_KBM), np.mean(str_KBM)) 
 
 #%%
@@ -111,98 +101,61 @@ plt.show()
 
 #%%
 #Average clustering coefficient
+
+#GM12878
 t5=tm.time()
 
-# Function to compute local clustering for a single node
-def local_clustering(node):
-    result = nx.clustering(G_GM, nodes=node, weight='weight')
-    return result
+clustering_results_GM,avg_clustering_parallel_GM=utils.parallel_clustering(G_GM)
 
-# List of nodes
-nodes = list(G_GM.nodes)
-
-if __name__ == "__main__":
-
-    # Use a pool to compute local clustering in parallel
-    with Pool(processes=4) as pool:
-
-        with tqdm(total=len(nodes)) as pbar:
-                clustering_resultsGM = []
-                for result in pool.imap_unordered(local_clustering, nodes):
-                    clustering_resultsGM.append(result)
-                    pbar.update(1)
-        
-        
-    # Compute the average clustering coefficient
-    avg_clustering_parallelGM = sum(clustering_resultsGM) / len(clustering_resultsGM)
-    print(avg_clustering_parallelGM)
-
-#%%
-
-# Function to compute local clustering for a single node
-def local_clustering(node):
-    result = nx.clustering(G_KBM, nodes=node, weight='weight')
-    return result
-
-# List of nodes
-nodes = list(G_KBM.nodes)
-
-if __name__ == "__main__":
-
-    # Use a pool to compute local clustering in parallel
-    with Pool(processes=4) as pool:
-
-        with tqdm(total=len(nodes)) as pbar:
-                clustering_resultsKBM = []
-                for result in pool.imap_unordered(local_clustering, nodes):
-                    clustering_resultsKBM.append(result)
-                    pbar.update(1)
-        
-        
-    # Compute the average clustering coefficient
-    avg_clustering_parallelKBM = sum(clustering_resultsKBM) / len(clustering_resultsKBM)
-    print(avg_clustering_parallelKBM)
-
-# %%
 # Save the results to a text file
 file_clustering = open("clustering_resultsGM.txt", "w")
 with open("clustering_resultsGM.txt", "w") as file:
-    for value in clustering_resultsGM:
+    for value in clustering_results_GM:
         file_clustering.write(f"{value}\n")  # Each float on a new line
 file_clustering.close()
 
+# Save as a NumPy binary file
+np.save("clustering_resultsGM.npy", clustering_results_GM)
 
 #%%
+#KBM7
+t5p=tm.time()
+
+clustering_results_KBM,avg_clustering_parallel_KBM=utils.parallel_clustering(G_KBM)
+
 file_clustering = open("clustering_resultsKBM.txt", "w")
 with open("clustering_resultsKBM.txt", "w") as file:
-    for value in clustering_resultsKBM:
+    for value in clustering_results_KBM:
         file_clustering.write(f"{value}\n")  # Each float on a new line
 file_clustering.close()
+
+np.save("clustering_resultsKBM.npy", clustering_results_KBM)
+
+# %%
+
+# Load the results from the text file
 
 #with open("clustering_resultsGM.txt", "r") as file:
 #    clustering_resultsGM= [float(line.strip()) for line in file]
 #    #print(clustering_resultsGM)
 #file.close()
 
-#%%
-# Save as a NumPy binary file
-np.save("clustering_resultsGM.npy", clustering_resultsGM)
-
-
-#%%
-np.save("clustering_resultsKBM.npy", clustering_resultsKBM)
+#with open("clustering_resultsKBM.txt", "r") as file:
+#    clustering_resultsKBM= [float(line.strip()) for line in file]
+#    #print(clustering_resultsKBM)
+#file.close()
 
 #%%
+
 # Load the NumPy binary file
-clustering_resultsGM= np.load("clustering_resultsGM.npy")
-clustering_resultsKBM= np.load("clustering_resultsKBM.npy")
+clustering_results_GM= np.load("clustering_resultsGM.npy")
+clustering_results_KBM= np.load("clustering_resultsKBM.npy")
 
 # %%
 #Histogram of clustering coefficients
-t6=tm.time()
 
 #conditions for log10: remove zeros
-cl_coeff_GM=np.array(clustering_resultsGM)
+cl_coeff_GM=np.array(clustering_results_GM)
 cl_coeff_GM_n0=cl_coeff_GM[cl_coeff_GM>0]
 
 plt.hist(cl_coeff_GM_n0, bins=300)
@@ -217,6 +170,8 @@ plt.show()
 
 #%%
 #Spectral analysis of matrices
+t6=tm.time()
+
 #GM12878
 eigenvalues_GM_norm, eigenvectors_GM_norm = LA.eigh(dataGM_normalized)
 print(np.max(eigenvalues_GM_norm))
@@ -253,6 +208,7 @@ plt.show()
 
 #%%
 #Spectral density comparison
+t7=tm.time()
 
 plt.hist(eigvals_GM[eigvals_GM<100], bins=150, density = True, histtype= 'step', label='GM12878')
 plt.hist(eigvals_KBM[eigvals_KBM<100], bins=150, density = True, histtype= 'step', label='KBM7')
@@ -274,18 +230,11 @@ for eigenvectors in [[eigenvectors_GM_norm,"GM12878"], [eigenvectors_KBM_norm,"K
         plt.show()
 
 #%%
-#modify indexing to locate chromoseomes
+#modify indexing to locate chromosomes for eigenvectors analysis
+t8=tm.time()
 
-def clean_indexing(matrix):
-    #add Y indexes corresponding to the range 2893-2952
-    ind=list(range(2893,2953))
-    for i in range(matrix.shape[0]):
-        if np.all(matrix[i] == 0) and i not in ind:
-            ind.append(i)
-    return ind
-
-ind_GM=clean_indexing(dataGM0)
-ind_KBM=clean_indexing(dataKBM0)
+ind_GM=utils.clean_indexing(dataGM0)
+ind_KBM=utils.clean_indexing(dataKBM0)
 
 ind_chr=[]
 dfchr=pd.read_csv(dir_data+"metadata_GM12878_KBM7.csv",header=0)
@@ -294,19 +243,17 @@ dfchr=pd.read_csv(dir_data+"metadata_GM12878_KBM7.csv",header=0)
 for i in range(len(dfchr)):
     ind_chr.append(list(range(dfchr["start"][i],dfchr["end"][i]+1)))
     
-chro = dict(zip(dfchr["chr"], ind_chr))  
+chro = dict(zip(dfchr["chr"], ind_chr)) 
 
 #remove the indexes of the Y chromosome and the isolated nodes
-for key, value_list in chro.items():
-    chro[key] = [item for item in value_list if item not in ind_GM]
-
-#substitute old values with new ones starting from zero and increasing
 a=0
 for key, value in chro.items():
+    chro[key] = [item for item in value if item not in ind_GM]
+    #substitute old values with new ones starting from zero and increasing
     if len(value)!=0:
         value=list(range(a,a+len(value)))
         a+=len(value)  
-        chro[key]=value
+        chro[key]=value    
 
 # %%
 #Eigenvectors analysis
@@ -332,6 +279,7 @@ for eigenvectors in [[eigenvectors_GM_norm,"GM12878"], [eigenvectors_KBM_norm,"K
 
 #%%
 #IPR
+t9=tm.time()
 
 #GM12878
 IPR_GM = np.sum(eigenvectors_GM_norm**4, axis=1)
@@ -365,48 +313,20 @@ for i in l_ranges:
     plt.show()
     
 #%%
+#Adjacency matrix
+t10=tm.time()
+
 #Adjacency matrix visualization
-
-def plot_adjacency_matrix(data, cell, N):
-    plt.imshow(data, cmap='plasma', interpolation='none') #plasma or gist_heat
-    plt.colorbar()
-    plt.title(f'Adjacency Matrix {cell}: with {N} values')
-    plt.show()
-    return
-
-plot_adjacency_matrix(dataGM_normalized, "GM12878", "all")
-plot_adjacency_matrix(dataKBM_normalized, "KBM7", "all")
+utils.plot_adjacency_matrix(dataGM_normalized, "GM12878", "all")
+utils.plot_adjacency_matrix(dataKBM_normalized, "KBM7", "all")
 
 #%%
 #Computing essential matrix for a different number of eigenvectors and eigenvalues
 
-def compute_essential_matrix(eigval, eigvec, N, cell):
-    # ordering in an absolute descending order 
-    idx = np.argsort(np.abs(eigval))[::-1]
-    # selecting top N eigenvalues and eigenvectors
-    eigval_topN = eigval[idx][:N]
-    eigvec_topN = eigvec[idx][:N]
-    
-    A_ess = np.zeros((len(eigval), len(eigval)))
-    
-    # compute the essential matrix using the formula
-    for n in range(N):
-        k_n = eigval_topN[n]
-        a_n = eigvec_topN[n]
-        
-        # compute the contribution of eigenvector n to the essential matrix
-        A_ess += k_n * np.outer(a_n, a_n)  
-    
-    #creating corresponding plot
-    plot_adjacency_matrix(A_ess, cell, N)
-
-    return 
-
 #GM12878
-compute_essential_matrix(eigenvalues_GM_norm, eigenvectors_GM_norm, 10, "GM12878")
-compute_essential_matrix(eigenvalues_GM_norm, eigenvectors_GM_norm, 15, "GM12878")
-compute_essential_matrix(eigenvalues_GM_norm, eigenvectors_GM_norm, 20, "GM12878")
-compute_essential_matrix(eigenvalues_GM_norm, eigenvectors_GM_norm, 25, "GM12878")
-
+utils.compute_essential_matrix(eigenvalues_GM_norm, eigenvectors_GM_norm, 10, "GM12878")
+utils.compute_essential_matrix(eigenvalues_GM_norm, eigenvectors_GM_norm, 15, "GM12878")
+utils.compute_essential_matrix(eigenvalues_GM_norm, eigenvectors_GM_norm, 20, "GM12878")
+utils.compute_essential_matrix(eigenvalues_GM_norm, eigenvectors_GM_norm, 25, "GM12878")
 
 # %%
